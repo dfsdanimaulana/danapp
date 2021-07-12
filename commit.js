38d1cfa8133192bc1db9dc308e5cfba94a3962d0
dfsdanimaulana <danimaulana9f@gmail.com>
2021-07-12 22:00:01 +0700

    membuat auto commit

---
+++ commit.js
@@ -0,0 +1,28 @@
+const {
+    exec
+} = require('child_process')
+
+let args = process.argv
+args.splice(0, 2)
+
+let str = args.join(' ')
+
+exec('git add .', cbAdd)
+
+function cbAdd (err, strout, stdin) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    exec(`git commit -m "${str}"`, cbCommit)
+}
+
+function cbCommit(err, strout, stdin) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('commit done msg : ', str);
+}
+
+// npm run commit -- message
