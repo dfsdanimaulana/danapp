67146923d3a18d71ed8734f27794132d9b58a7bf
dfsdanimaulana <danimaulana9f@gmail.com>
2021-07-17 15:58:30 +0700

    selesai page profile

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,7 +1,21 @@
+/* eslint-disable no-undef */
 const moment = require('moment')
 const time = moment().format('hh:mm A')
 
 const { Chat } = require('../models/chat.model')
+
+// const addData = (req, res ) => {
+  // const datas = new Chat({
+  //   img:'images/Dani.jpg',
+  //   username:'Dani Maulana',
+  //   message: 'Hello world!',
+  //   timeSend:time
+  // })
+  // datas.save((err, data)=>{
+  //   if(err) console.error(err)
+  //   console.log(data)
+  // })
+// }
 
 const view = (req, res) => {
     
--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,9 +1,17 @@
+/* eslint-disable no-undef */
 exports.view = (req, res) => {
+  const data = {
+    img: 'images/Dani.jpg',
+    username: 'Dani maulana',
+    email:'danimaulana9f@gmail.com',
+    info: 'aink bogoh ka sia'
+  }
   const params = {
     layout: 'layouts/html',
     title: 'Profile Page',
     style: 'profile',
     script: 'page',
+    data
   }
   res.render('profile', params)
 }
