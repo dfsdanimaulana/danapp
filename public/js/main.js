e015176891f5311179742f066cfe330defb01fda
dfsdanimaulana <danimaulana9f@gmail.com>
2021-07-27 10:02:47 +0700

    add outside listener

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,37 @@
 const btn = document.querySelector('.plus')
-btn.addEventListener('click', () => {
-  const menu = document.querySelector('.btn-container')
-  menu.classList.toggle('open')
-})
+const menu = document.querySelector('.btn-container')
+
 const nav_btn_icon = document.querySelector('.nav-btn-icon')
+const nav_btn = document.querySelector('.nav-btn')
+
+btn.addEventListener('click', () => {
+  menu.classList.toggle('open')
+  nav_btn.classList.remove('nav-open')
+})
+
 nav_btn_icon.addEventListener('click', () => {
-  const nav_btn = document.querySelector('.nav-btn')
   nav_btn.classList.toggle('nav-open')
+  menu.classList.remove('open')
+})
+
+// add two or more event listener in a single element
+const evenListener = ['click', 'scroll']
+
+evenListener.forEach((ev) => {
+  // outside click detect
+  document.addEventListener(ev, (out) => {
+    let clickedElement = out.target
+    do {
+      if (clickedElement == nav_btn) {
+        // this is inside click. do nothing just return
+        return
+      }
+      if (clickedElement == btn) {
+        return
+      }
+      clickedElement = clickedElement.parentNode
+    } while (clickedElement)
+    nav_btn.classList.remove('nav-open')
+    menu.classList.remove('open')
+  })
 })
