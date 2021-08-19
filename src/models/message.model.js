13991f1ddaaa48a221af492ea13d26bfb5344c69
dfsdanimaulana <danimaulana9f@gmail.com>
2021-08-19 17:12:30 +0700

    finish chat dont change again

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const mongoose = require('mongoose')
-const { Schema } = mongoose
+const {
+    Schema
+} = mongoose
 const moment = require('moment')
 
 const opts = {
     // Make Mongoose use Unix time (seconds since Jan 1, 1970)
-    timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+    timestamps: {
+        currentTime: () => Math.floor(Date.now() / 1000)
+    },
 }
 const msgSchema = new Schema(
     {
@@ -22,16 +26,20 @@ const msgSchema = new Schema(
             required: true,
             type: String,
         },
-        timeSend: {
-            type: String,
-            default: moment().format('hh:mm A'),
-        },
-    },
-    opts
-)
+        isRead: {
+            type: Boolean,
+            default: false,
+            },
+            timeSend: {
+                type: String,
+                default: moment().format('hh:mm A'),
+                },
+            },
+            opts
+        )
 
-const Message = mongoose.model('Message', msgSchema)
+        const Message = mongoose.model('Message', msgSchema)
 
-module.exports = {
-    Message,
-}
+        module.exports = {
+            Message,
+        }
