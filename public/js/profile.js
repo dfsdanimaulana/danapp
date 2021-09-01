2b8f12f077a050cb725d329c5ee0a43740b38ee7
dfsdanimaulana <danimaulana9f@gmail.com>
2021-09-01 18:17:51 +0700

    membuat image preview saat menginput image sebelum di submit

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,7 +4,7 @@
 const navmenu = document.querySelector('.nav-menu')
 const icon = document.querySelector('.bi.bi-three-dots-vertical')
 
-icon.addEventListener('click', ()=> {
+icon.addEventListener('click', () => {
     navmenu.classList.add('open')
 })
 
@@ -26,11 +26,10 @@ evenListener.forEach((ev) => {
             }
             clickedElement = clickedElement.parentNode
         } while (clickedElement)
-            navmenu.classList.remove('open')
+        navmenu.classList.remove('open')
     })
 })
 // nav menu end
-
 
 // modal start
 const modalContainer = document.querySelector('.modal-container')
@@ -42,16 +41,14 @@ const header = document.querySelector('.modal-header h3')
 const input = document.querySelector('.modal-input')
 const updater = document.querySelector('.updater')
 
-
-openModal.forEach(open => {
-    open.addEventListener('click',
-        ()=> {
-            modalContainer.style.display = 'grid'
-            modal.style.display = 'block'
-            header.innerHTML = `Set ${open.dataset.headername}`
-            input.value = open.dataset.value
-            updater.value = open.dataset.headername
-        })
+openModal.forEach((open) => {
+    open.addEventListener('click', () => {
+        modalContainer.style.display = 'grid'
+        modal.style.display = 'block'
+        header.innerHTML = `Set ${open.dataset.headername}`
+        input.value = open.dataset.value
+        updater.value = open.dataset.headername
+    })
 })
 
 closeModal.onclick = () => {
@@ -67,7 +64,7 @@ modalContainer.addEventListener('click', (out) => {
         }
         clickedElement = clickedElement.parentNode
     } while (clickedElement)
-        modalContainer.style.display = 'none'
+    modalContainer.style.display = 'none'
 })
 
 // modal end
@@ -75,7 +72,7 @@ modalContainer.addEventListener('click', (out) => {
 const currentUser = document.querySelector('#current_user').value
 const thisUser = document.querySelector('#this_user').value
 if (currentUser !== thisUser) {
-    openModal.forEach(el => {
+    openModal.forEach((el) => {
         el.style.visibility = 'hidden'
     })
 }
@@ -89,10 +86,50 @@ const modal_img = document.querySelector('.modal-img')
 const modal_img_i = document.querySelector('.modal-img i')
 const modal_img_form = document.querySelector('.modal-img form')
 
-profile_edit.addEventListener('click',()=>{
+profile_edit.addEventListener('click', () => {
     modal_img_container.style.display = 'block'
 })
 
-modal_img_i.addEventListener('click',()=>{
+modal_img_i.addEventListener('click', () => {
     modal_img_container.style.display = 'none'
 })
+
+modal_img_container.addEventListener('click', (out) => {
+    let clickedElement = out.target
+    do {
+        if (clickedElement == modal_img) {
+            // this is inside click. do nothing just return
+            return
+        }
+        clickedElement = clickedElement.parentNode
+    } while (clickedElement)
+    modal_img_container.style.display = 'none'
+})
+
+// input image logic
+
+// show selected image before submiting
+// get input element
+
+function fileValidation(id) {
+    const fileInput = document.getElementById(id)
+    const filePath = fileInput.value
+    const allowedExt = /(\.jpg|\.jpeg|\.png)$/i
+
+    if (!allowedExt.exec(filePath)) {
+        alert('file not valid!')
+        fileInput.value = ''
+        return false
+    } else {
+        // image preview
+        if (fileInput.files && fileInput.files[0]) {
+            const reader = new FileReader()
+            reader.onload = (e) => {
+                document
+                    .querySelector('#profile-img')
+                    .setAttribute('src', e.target.result)
+            }
+            reader.readAsDataURL(fileInput.files[0])
+        }
+    }
+}
